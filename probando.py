434aca314a23472880ede520bd19d51072ecb8a3
Maximilian Reutter <maximilianreutter@gmail.com>
2022-10-09 21:25:28 +0200

    agregar .py y main
    resolviendo conflictos de merge con el Pat

--- probando.py
+++ probando.py
@@ -5,4 +5,17 @@ def publicidad():
     print("100.000 adena / 10 USD / PM me!")
 
 def despedirse():
+<<<<<<< Updated upstream
     print("Bueno chao.")
+=======
+    print("Bueno chao.")
+
+class Dog:
+    id = 0
+
+    def __init__(self, name):
+        self.name =name
+
+    def lick(self, person_name):
+        print(f"{self.name} licks {person_name}. \"Lick Lick Lick\" *Lick* *Lick* *Lick*")
+>>>>>>> Stashed changes
